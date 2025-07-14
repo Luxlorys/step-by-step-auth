1c38a4dbd7fb5c557607d18aed972f0ae4c03e09
Luxlorys <bossbyrcev221@gmail.com>
2025-07-14 23:23:11 +0300

    chore: ui fixes with scroll

--- src/layouts/DashboardLayout.tsx
+++ src/layouts/DashboardLayout.tsx
@@ -7,21 +7,19 @@ const DashboardLayout = () => {
   const { isSuperAdmin } = useAuth();
   
   return (
-    <div className="h-screen bg-[#F2F2F2] p-3 overflow-hidden">
+    <div className="h-screen bg-[#F2F2F2] p-3 overflow-hidden"> {/* Changed to overflow-hidden */}
       <div className="h-full flex gap-3">
         {/* Sidebar */}
         <Sidebar isSuperAdmin={isSuperAdmin} />
         
         {/* Main Content */}
-        <div className="h-full flex-1 flex flex-col gap-3">
+        <div className="h-full flex-1 flex flex-col gap-3 min-w-0"> {/* Added min-w-0 */}
           {/* Top Bar */}
           <TopBar />
           
           {/* Page Content */}
-          <main className="flex-1 bg-white rounded-xl p-6 overflow-auto min-h-0">
-            <div className="min-w-max">
-              <Outlet />
-            </div>
+          <main className="flex-1 bg-white rounded-xl p-6 overflow-auto"> {/* This stays overflow-auto */}
+            <Outlet />
           </main>
         </div>
       </div>
